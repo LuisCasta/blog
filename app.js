c3933eca5397779949646fd1550acc08089fbcb2
LuisCasta <luis.alankasta@hotmail.com>
2022-12-05 08:57:50 -0600

    documentation swagger

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const app = express()
 const routes = require('./router/index')
-const { uploadFile } = require('./config/s3.js')
+const {swaggerDocs : swaggerDocsV1} = require("./router/swagger")
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
@@ -19,29 +19,8 @@ app.use(bodyParser.json());
 app.use(cors({origin:"*"}))
 app.use("/api/v1", routes());
 
-app.post('/uploadImage', (req, res, next) => {
-    const form = formidable({ multiples: true });
-  
-    form.parse(req,async (err, fields, files) => {
-      if (err) {
-        next(err);
-        return;
-      }
-
-      let pathFile = {};
-
-      pathFile.path = files.files.filepath;
-      pathFile.originalFilename = files.files.originalFilename;
-
-      const result = await uploadFile(pathFile)
-      const url = `https://blogcuentame.s3.us-east-2.amazonaws.com/${pathFile.originalFilename}`;
-
-      res.json({ url });
-
-    });
-  });
-
 app.listen(PORT, () => {
     console.log(`Server running on ${HOST}:${PORT}`)
+    swaggerDocsV1(app,PORT)
 });
 
--- router/index.js
+++ router/index.js
@@ -9,6 +9,255 @@ const apiRouter = () => {
     const entradaRouter = EntradaRouter();
     const comentarioRouter = ComentarioRouter();
     
+    /**
+     * @openapi
+     * /api/v1/entrada:
+     *   get:
+     *     summary: Obtenet entrada por id
+     *     tags: 
+     *      - entrada
+     *     requestBody:
+     *       required: true
+     *       content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                id_entrada:
+     *                  type: integer
+     *     responses:
+     *       200:
+     *         description: ok
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 data:
+     *                    type: object
+     *       400:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *       500:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *
+     *   post:
+     *     summary: Crear entrada
+     *     tags: 
+     *      - entrada
+     *     requestBody:
+     *       required: true
+     *       content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                titulo: 
+     *                  type: string
+     *                cuerpo:
+     *                  type: string
+     *     responses:
+     *       200:
+     *         description: ok
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 data:
+     *                    type: object
+     *       400:
+     *         description: error 
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *       500:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *
+     * 
+     *   put:
+     *     summary: Actualizar entrada
+     *     tags: 
+     *      - entrada
+     *     requestBody:
+     *       required: true
+     *       content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                id_entrada:
+     *                  type: integer
+     *                titulo: 
+     *                  type: string
+     *                cuerpo:
+     *                  type: string
+     *     responses:
+     *       200:
+     *         description: ok
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 data:
+     *                    type: object
+     *       400:
+     *         description: error query
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *       500:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *
+     * 
+     *   delete:
+     *     summary: Elininar entrada
+     *     tags: 
+     *      - entrada
+     *     requestBody:
+     *       required: true
+     *       content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                id_entrada:
+     *                  type: integer
+     *     responses:
+     *       200:
+     *         description: ok
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 data:
+     *                    type: object
+     *       400:
+     *         description: error query
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *       500:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *
+     * get:
+     *     summary: Enlistar todas las entradas
+     *     tags: 
+     *      - entrada
+     *     requestBody:
+     *       required: true
+     *       content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                id_entrada:
+     *                  type: integer
+     *     responses:
+     *       200:
+     *         description: ok
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 data:
+     *                    type: object
+     *       400:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     *       500:
+     *         description: error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                    type: integer
+     *                 message:
+     *                    type: string
+     * 
+     */
+    
     routes.use("/entrada",entradaRouter);
     routes.use("/comentario",comentarioRouter);
 
